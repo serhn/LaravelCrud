3b8ebf423ef86dff76455be15fa9d2929aed9e06
Serhii Nikolaichuk <serh@limit.com.ua>
2019-11-18 21:37:16 +0200

    add autofocus

--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -22,7 +22,7 @@
 </div>
 <div class="">
     <form method="GET" class="w-100 d-flex">
-      <input name="q" class="form-control form-control-dark" type="text" placeholder="Поиск"
+      <input name="q" autofocus="autofocus" class="form-control form-control-dark" type="text" placeholder="Поиск"
     aria-label="Поиск" value="{{Request::input('q')}}">
       <button data-section="europe" type="submit" class="btn btn-primary ml-1">
         <i class="fas fas fa-search"></i>
