3a9b3bb0e54f22fe722f108e605cc763c71069da
Serhii Nikolaichuk <serh@limit.com.ua>
2019-11-26 02:49:36 +0200

    add phone field

---
+++ resources/views/crud/fields/phone.blade.php
@@ -0,0 +1,19 @@
+<div class="form-group row">
+    <label for="{{$key}}" class="col-sm-2 col-form-label">{{$item["name"]}}</label>
+    <div class="input-group col-sm-10">
+        <div class="input-group-prepend">
+            <div class="input-group-text">+</div>
+        </div>
+        <input type="tel"
+        pattern="[0-9]{10,15}"
+        class="form-control @error($key) is-invalid @enderror" name="{{$key}}" id="{{$key}}"
+        value="{{old($key,@$row->$key)}}"
+        {!!isset($item["maxlength"])?'maxlength="'.$item["maxlength"].'"':''!!}
+        {!!isset($item["placeholder"])?'placeholder="'.$item["placeholder"].'"':''!!}>
+        @error($key)
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+    </div>
+</div>
