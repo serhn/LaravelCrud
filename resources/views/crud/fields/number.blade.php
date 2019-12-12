46729f60586fa69f97420435bf198113383986e6
Serhii Nikolaichuk <serh@limit.com.ua>
2019-12-13 00:46:07 +0200

    add number validate info

--- resources/views/crud/fields/number.blade.php
+++ resources/views/crud/fields/number.blade.php
@@ -1,9 +1,14 @@
 <div class="form-group row">
     <label for="{{$key}}" class="col-sm-2 col-form-label">{{$item["name"]}}</label>
     <div class="col-sm-10">
-        <input type="number" class="form-control" name="{{$key}}" id="{{$key}}"  
+        <input type="number" class="form-control @error($key) is-invalid @enderror" name="{{$key}}" id="{{$key}}"  
         value="{{old($key,@$row->$key)}}"
         {!!isset($item["maxlength"])?'maxlength="'.$item["maxlength"].'"':''!!}
         {!!isset($item["placeholder"])?'placeholder="'.$item["placeholder"].'"':''!!}>
+        @error($key)
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
     </div>
 </div>
